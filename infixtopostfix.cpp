f1db80450368e707897f224a95400deda82d2050
Jannat Rashid <97726066+jannatRashid@users.noreply.github.com>
2022-10-12 02:12:13 -0700

    Update infixtopostfix.cpp

--- infixtopostfix.cpp
+++ infixtopostfix.cpp
@@ -1,48 +1,86 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
 using namespace std;
-
-int precedence(char op) 
-{ 
-    if(op == '^') 
-    return 3; 
-    else if(op == '*' || op == '/') 
-    return 2; 
-    else if(op == '+' || op == '-') 
-    return 1; 
-    else
-    return 0; 
-} 
-
-int main()
-
+int IsOperator(char op)
 {
-    char stack[10];
-    int top=-1;
-    int j=0;
+    {
+        if (op == '^')
+            return 3;
+        if (op == '*' ||  (op == '/') ||  (op == '%'))
+            return 2;
+        
+        if (op == '+' || (op == '-'))
+            return 1;
+       
+        if (op == '(' || (op == '(') )
+            return 4;
+        
+    }
 
+    return 0;
+}
+int main()
+{
     char infix[10];
-    strcpy(infix,"a+b");
-
+    // strcpy(infix, "b-c/d");
+    cout<<"enter infix expression:";
+    cin>>infix;
+    char stack[10];
+    int top = -1;
+    int j = 0;
     char postfix[10];
-    strcpy(postfix," ");
+    strcpy(postfix, "");
 
     for (int i = 0; i < strlen(infix); i++)
     {
-        if (precedence(infix[i]==0))
-        {
-            postfix[j++]=infix[i];
-        }
-        else if (stack[++top]==infix[i])
-        {
-            
-        }
-        
-    }
-    postfix[j++]=stack[top--];
-    postfix[j++]='\0';
-    printf("%s",postfix);
-    
+           if(infix[i] == '(')      
+    {  stack[++top] = '(';    continue; }
 
-        return 0;
+
+
+        if(infix[i] == ')')      
+    {    while(stack[top]!=')') 
+           postfix[j++] =  stack[top--]; 
+         top++;  // to remove ')'
+        continue;
+    }
+
+
+
+
+
+
+
+
+
+
+        if (IsOperator(infix[i]) == 0)
+            postfix[j++] = infix[i];
+        else
+        {   
+            
+            if(stack[top] == '(' || top==-1) 
+             {  stack[++top] = infix[i] ; continue; }
+             // operator
+
+
+
+            while (top != -1 && IsOperator(infix[i]) <= IsOperator(stack[top]))
+                postfix[j++] = stack[top--];
+
+            stack[++top] = infix[i];
+        }
+        // if (top == -1 || stack[top] == '(')
+        //     stack[top++] = infix[i];
+        // else if (infix[i] == ')')
+        // {
+        //     while (stack[top] != '(')
+        //         postfix[j++] = stack[top--];
+        // }
+    }
+    while (top >= 0)
+        postfix[j++] = stack[top--];
+    postfix[j++] = '\0';
+    printf("%s", postfix);
 }
